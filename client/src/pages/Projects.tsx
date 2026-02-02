c159ed9fb339ce627e016e4f2d30d4393101f670
Adesh <adeshsonpure@gmail.com>
2026-02-03 02:34:51 +0530

    A

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import type { Project } from '../types'
-import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, Fullscreen, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, Sidebar, Smartphone, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react'
-import { dummyProjects, dummyConversations } from '../assets/assets'
+import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, Fullscreen, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, Smartphone, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react'
+import { dummyProjects, dummyConversations, dummyVersion } from '../assets/assets'
 import ProjectPreview, { type ProjectPreviewRef } from '../components/ProjectPreview'
+import Sidebar from '../components/Sidebar'
 
 const Projects = () => {
   const {projectId} = useParams()
@@ -24,7 +25,7 @@ const Projects = () => {
      const project = dummyProjects.find(project =>project.id === projectId)
     setTimeout(()=>{
       if(project){
-        setProject({...project,conversation: dummyConversations});
+        setProject({...project,conversation: dummyConversations,versions:dummyVersion});
         setLoading(false)
         setIsGenerating(project.current_code ? false : true)
       }
@@ -107,7 +108,7 @@ const Projects = () => {
       </div>
       </div>
       <div className='flex-1 flex overflow-auto'>
-             <Sidebar isMenuOpen={isMenuOpen} project={project} setProject={(p)=>setProject(p)} isGenerating={isGenerating} setIsGenerating={setIsGenerating}/>
+             <Sidebar isMenuOpen ={isMenuOpen} project={project} setProject={(p)=>setProject(p)} isGenerating={isGenerating} setIsGenerating={setIsGenerating}/>
               <div className='flex-1 p-2 pl-0'>
                 <ProjectPreview ref={previewRef} project={project} isGenrating={isGenerating} device={device}/>
               </div>
